2c5178058237d72343173c61ab3b22166528586a
XIAC <x.i.a.c.1504@gmail.com>
2022-04-25 11:42:21 -0400

    configuraciones adicionales

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const aplicacion = express();
 const mongoose = require('./database/mongoose');
 aplicacion.listen(5500, () => {
-    console.log("el puerto esta iniciando en 5500");
+    console.log("el puerto esta iniciando en 5500 fff");
 });
