f6b6c527edf83e10420b4d786d67a86ca0fc2834
XIAC <x.i.a.c.1504@gmail.com>
2022-04-25 11:45:22 -0400

    configuracion adicional

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/task', {useNewUrlParser:true, useUnifiedTopology: true} )
+mongoose.connect('mongodb://127.0.0.1:27017/tarea', {useNewUrlParser:true, useUnifiedTopology: true} )
     .then(() => console.log("mongoose funcionando!!") )
     .catch( (error) => console.log(error));
 module.exports = mongoose;
